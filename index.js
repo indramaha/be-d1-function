2d3cf106132ea09710936ac9e17d6e74b85abd1a
Tugus <idabgusindra01@gmail.com>
2022-12-17 00:43:59 +0800

    first init and learn function for backend js

---
+++ index.js
@@ -0,0 +1,31 @@
+// function sum(a, b){
+//     const resultSum = a + b
+//     return resultSum
+// }
+
+// const result = sum(10, 50)
+
+// console.log(result)
+
+//soal nomor 1
+// function luasLingkaran(r){
+//     const phi = 3.14
+//     const result = phi * r * r
+//     return(result)
+// }
+
+// const resultLuasLingkaran = luasLingkaran(10)
+
+// console.log(resultLuasLingkaran)
+
+//soal nomor 2
+// hitung luas balok
+// function luasBalok(p, l, t){
+//     const result = p*l*t
+//     return(result)
+// }
+
+// const showResult = luasBalok(5, 2, 2)
+// console.log(showResult)
+
+//soal nomor 3
